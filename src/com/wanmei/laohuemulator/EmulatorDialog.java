4133ad4eaea8571ca6c14337afa6ca8139e57c31
vigor <vigor@vigor.(none)>
2013-12-03 17:27:14 +0800

    Submit on 20131203.

---
+++ src/com/wanmei/laohuemulator/EmulatorDialog.java
@@ -0,0 +1,14 @@
+package com.wanmei.laohuemulator;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class EmulatorDialog extends AlertDialog {
+
+	protected EmulatorDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ src/com/wanmei/laohuemulator/settings/UpdataDialog.java
@@ -0,0 +1,13 @@
+package com.wanmei.laohuemulator.settings;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class UpdataDialog extends Dialog {
+
+	public UpdataDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
